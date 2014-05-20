23047dd954c34ceffd009208c9d37453902c4016
Tim Brigham <timb@cfcausa.org>
2014-05-20 10:51:53 -0500

    Update the plugin ID numbers to a a new range. Trying to set up some community friendly numbering.

--- alienvault/dionaealog.sql
+++ alienvault/dionaealog.sql
@@ -1,14 +1,14 @@
-﻿DELETE FROM plugin WHERE id = "9902";
-DELETE FROM plugin_sid where plugin_id = "9902";
+﻿DELETE FROM plugin WHERE id = "10000";
+DELETE FROM plugin_sid where plugin_id = "10000";
 
 # Register the plugin
-INSERT IGNORE INTO plugin (id, type, name, description, vendor, product_type ) VALUES (9902, 1, 'dionaealogs', 'Dionaea Logs', 'TimB', 1 );
+INSERT IGNORE INTO plugin (id, type, name, description, vendor, product_type ) VALUES (10000, 1, 'dionaealogs', 'Dionaea Logs', 'TimB', 1 );
 
 # And the individual SID values
-INSERT IGNORE INTO plugin_sid (plugin_id, sid, category_id, subcategory_id, class_id, name, priority, reliability) VALUES (9902, 1, 19, 225, NULL, 'Dionaea rejected connection', 1, 1);
-INSERT IGNORE INTO plugin_sid (plugin_id, sid, category_id, subcategory_id, class_id, name, priority, reliability) VALUES (9902, 2, 19, 225, NULL, 'Dionaea accepted connection', 1, 1);
+INSERT IGNORE INTO plugin_sid (plugin_id, sid, category_id, subcategory_id, class_id, name, priority, reliability) VALUES (10000, 1, 19, 225, NULL, 'Dionaea rejected connection', 1, 1);
+INSERT IGNORE INTO plugin_sid (plugin_id, sid, category_id, subcategory_id, class_id, name, priority, reliability) VALUES (10000, 2, 19, 225, NULL, 'Dionaea accepted connection', 1, 1);
 
-INSERT IGNORE INTO plugin_sid (plugin_id, sid, category_id, subcategory_id, class_id, name, priority, reliability) VALUES (9902, 99, 19, 225, NULL, 'Dionaea attack collected', 3, 5);
+INSERT IGNORE INTO plugin_sid (plugin_id, sid, category_id, subcategory_id, class_id, name, priority, reliability) VALUES (10000, 99, 19, 225, NULL, 'Dionaea attack collected', 3, 5);
 # Download events
 
 
